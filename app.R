90323d11f9e740297314c1536b52f9683e996753
Wen Li <joycey.li@mail.utoronto.ca>
2022-03-08 18:40:21 +0800

    Read all relevant dataframe and define the basic module for each page

--- app.R
+++ app.R
@@ -14,8 +14,37 @@ library(magrittr)
 
 
 # load our data -----------------------------------------------------------
-can_df <- read_csv('data/covid19.csv')
-can_df$date <- as.Date(can_df$date, format = "%d-%m-%Y")
+# national level statistics
+scotland_df <- read_csv('data/scotland.csv')
+england_df <- read_csv('data/england.csv')
+wales_df <- read_csv("data/wales.csv")
+north_df <- read_csv("data/north_ireland.csv")
+
+# city level statistics
+belfast_df <- read_csv("data/belfast.csv")
+belfast_df$hospitalCases = north_df$hospitalCases
+cardiff_df <- read_csv("data/cardiff.csv")
+cardiff_df$hospitalCases = wales_df$hospitalCases
+edinburgh_df <- read_csv("data/edinburgh.csv")
+edinburgh_df$hospitalCases = scotland_df$hospitalCases
+birmingham_df <- read_csv("data/birmingham.csv")
+birmingham_df$hospitalCases = england_df$hospitalCases
+bristol_df <- read_csv("data/bristol.csv")
+bristol_df$hospitalCases = england_df$hospitalCases
+cambridge_df <- read_csv("data/cambridge.csv")
+cambridge_df$hospitalCases = england_df$hospitalCases
+glasgow_df <- read_csv("data/glasgow.csv")
+glasgow_df$hospitalCases = scotland_df$hospitalCases
+
+belfast_vac_df <- read_csv("data/belfast_vac.csv")
+cardiff_vac_df <- read_csv("data/cardiff_vac.csv")
+edinburgh_vac_df <- read_csv("data/edinburgh_vac.csv")
+birmingham_vac_df <- read_csv("data/birmingham_vac.csv")
+bristol_vac_df <- read_csv("data/bristol_vac.csv")
+cambridge_vac_df <- read_csv("data/cambridge_vac.csv")
+glasgow_vac_df <- read_csv("data/glasgow_vac.csv")
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -24,31 +53,45 @@ ui <- fluidPage(
     titlePanel("Compnay X's COVID-19 Updates Dashboard" ),
     
     tabsetPanel(
-        tabPanel("Home", htmlOutput("hometext"), ),
+        tabPanel("Home", htmlOutput("homePage"), ),
         tabPanel("Case Counts"), 
-        tabPanel("Vaccine Impact")
+        tabPanel("Vaccine Impact", htmlOutput("vaccinePage"))
     
 ))
 
+vaccinePage <- fluidPage(
+    titlePanel("Vaccine Information"),
+    
+    mainPanel(
+        h3("England"),
+        h3("North Ireland"),
+        h3("Wales"),
+        h3("Scotland")
+    )
+)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    output$hometext <- (
-        renderText({"
-        <h3>Objective</h3>
-        The objective of this dashboard is to provide Company X Executives, 
+homePage <- fluidPage(
+    mainPanel(
+        h3("Objective"),
+        p("The objective of this dashboard is to provide Company X Executives, 
         with a COVID-19 dashboard, that clearly shows the current state of the
-        pandemic specfically across regions Compnay X does business in. \n
-        
-        <h3>Intended Use</h3>
-        The dashboard can be used to monitor the current status of the pandemic 
+        pandemic specfically across UK cities Compnay X does business in. \n"),
+        h3("Intended Use"),
+        p("The dashboard can be used to monitor the current status of the pandemic 
         with respect to case counts and vaccine impacts. The dashboard will provide
         alerts when daily cases in relevant regions, increase by a significant amount 
         over the last week. This can alert executives to closely monitor and potentially 
         make changes to local policies. The dashboard can also be used for supporting longer 
-        term decisions around vaccine mandates acording to the Vaccine impact tab. 
-            "})
+        term decisions around vaccine mandates acording to the Vaccine impact tab.")
     )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$homePage <- renderUI(homePage)
+    
+    output$vaccinePage <- renderUI(vaccinePage)
     
 }
 
